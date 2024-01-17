3bbb604fb23d68805279ad975468fe9444fcc56f
cgv <cgv@cgv>
2024-01-17 14:57:07 +0000

    add argparse function

--- create_split_file.py
+++ create_split_file.py
@@ -2,10 +2,27 @@ import os
 import glob
 import random
 
+import argparse
+
+parser = argparse.ArgumentParser(description='')
+
+parser.add_argument('--data_path', type=str,
+                    help='data path')
+
+args = parser.parse_args()
+
+
+random.seed(999) # fix seed
+
 # parent directory that contains all folders
 # sub folders should directly contain image files
-path = input("path: ")
-split_ratio = input("split ratio [train:val] (ex 7:3): ")
+# path = input("path: ")
+# path = "assets/frames"
+
+path = args.data_path
+
+# split_ratio = input("split ratio [train:val] (ex 7:3): ")
+split_ratio = "9:1"
 a, b = split_ratio.split(":")
 a = int(a)
 b = int(b)
@@ -17,33 +34,38 @@ if not(os.path.exists(path) and os.path.isdir(path)):
     exit()
 
 path = os.path.normpath(path)
-os.chdir(path)
+# print(os.listdir(path))
+# print(os.listdir(path))
 
-odom_test_file_path = "odom_test_{}.txt"
+
+# odom_test_file_path = "odom_test_{}.txt"
 train_file_path = os.path.join("train_files.txt")
 val_file_path = os.path.join("val_files.txt")
 
+print(train_file_path)
 
 all_file_list = []
 
-for folder in os.listdir(path):
+folders = os.listdir(path)
+os.chdir(path)
+for folder in folders:
     if os.path.isdir(folder):
+        # print('yes')
         file_list = sorted(glob.glob(os.path.join(folder, "*.jpg")))
-        print(len(file_list))
+        # print(len(file_list))
         # f.writelines(file_list)
         # format is <folder> <number> <r | l>
 
-        # for individual file
-        with open(odom_test_file_path.format(folder), 'w') as f:
-            for item in file_list:
-                f.write(folder + " " + str(int("".join(os.path.splitext(os.path.basename(item))[:-1]))) + " " + "l" + "\n")
+        # # for individual file
+        # with open(odom_test_file_path.format(folder), 'w') as f:
+        #     for item in file_list:
+        #         f.write(folder + " " + str(int("".join(os.path.splitext(os.path.basename(item))[:-1]))) + " " + "l" + "\n")
         
         # for train and val files
         # need to remove first and last one because of frame_index [0, -1, 1]
         file_list = file_list[1:-1]
         for item in file_list:
             all_file_list.append(folder + " " + str(int("".join(os.path.splitext(os.path.basename(item))[:-1]))) + " " + "l" + "\n")
-
 
 random.shuffle(all_file_list)
 file_len = len(all_file_list)
@@ -58,5 +80,6 @@ with open(val_file_path, 'w') as f:
     for item in all_file_list[split_index:]:
         f.write(item)
 
+print("split file created in data path")
 
 # D:\CGVL\DepthEstimation\HandongDataset\camera_eun\images
